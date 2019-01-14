900ac236aec2f7cc5f27dec36ea5a4b1adbe6c64
arturo12018 <36450800+arturo12018@users.noreply.github.com>
2019-01-13 22:09:56 -0600

    Add files via upload

--- proyectocur.py
+++ proyectocur.py
@@ -1,3 +1,8 @@
+def op8ad(x):
+    print("Administrativo:",x[0],"\nApellido:",x[2],"\nEdad:",x[4],"\nSalario:",x[3], "\nContraseña:",x[1])
+    pass
+
+
 opcion=5
 while  opcion!=3:
     opcion=int(input("----SISTEMA DE VENTAS----\n   1. Administrador \n   2. Clinte  \n   3. Salir \nopcion: "))
@@ -6,7 +11,25 @@ while  opcion!=3:
         while op!=2:
             op=int(input("---Administrador---\n    1. Ingresar\n    2. Regresar\nopcion:"))
             if op==1:
-                print("hola")
+                administrador1=["Arturo","123"]
+                administrador2=["Luis","123"]
+                x=[]
+                x.append(input("Administrador:  "))
+                x.append(input("Contraseña:  "))
+                if administrador1==x:  
+                    administrador1=["Arturo","123","Muñoz","2000$",19]
+                    x=administrador1
+                    
+                if administrador2==x:
+                    administrador2=["Luis","123","Ortiz","2000$",19]
+                    x=administrador2
+                    
+                    
+                    
+                if administrador1!=x and administrador2!=x :
+                    print("administrador o contraseña esta mal")
+                    
+                     
             if op!=2 and op!=1:
                  print("\nopcion invalida")
         
